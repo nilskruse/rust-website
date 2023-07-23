0cd061d6e9a7ecd45ba1b9ebf70729076b5c5b2b
Nils Kruse <nilskruse97@gmail.com>
2023-07-23 17:45:17 +0200

    sync

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,48 @@
+use gloo::console;
+use js_sys::Date;
+use yew::{html, Component, Context, Html};
+
+// Define the possible messages which can be sent to the component
+pub enum Msg {
+    Increment,
+    Decrement,
+}
+
+pub struct App {
+    value: i64, // This will store the counter value
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Increment => {
+                self.value += 1;
+                console::log!("plus one"); // Will output a string to the browser console
+                true // Return true to cause the displayed change to update
+            }
+            Msg::Decrement => {
+                self.value -= 1;
+                console::log!("minus one");
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let text = "hello";
+        html! {
+            text
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
